759ea7eb86e23a1b7880a30cc7e754416daff393
ConnorMul <mulhollandconnor@icloud.com>
2020-12-09 13:10:47 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(arr, eventName) {
+    let thanksArray = []
+    for(let i = 0; i < arr.length; i++) {
+        thanksArray.push(`Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`);
+    
+    };
+    return thanksArray;
+};
+
+function countDown(int) {
+    while (int > 0) {
+    console.log(int);
+    int -= 1;
+    };
+    console.log(int)
+};
